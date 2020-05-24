3fdac942b32ad4468050ea5f7290d079645a0e6a
justsomeguy <thenaders3@gmail.com>
2020-05-24 11:23:59 -0700

    Updates

---
+++ precache-manifest.fb42863a815c6e721405d1d9a167c881.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73a42e59dd99f28c56c44cffe3d15c5",
+    "url": "/compound-channels/index.html"
+  },
+  {
+    "revision": "5f1bfdd3e22436547add",
+    "url": "/compound-channels/static/css/main.b77eeacf.chunk.css"
+  },
+  {
+    "revision": "989f3c3e55b305ed9b63",
+    "url": "/compound-channels/static/js/2.eddebabf.chunk.js"
+  },
+  {
+    "revision": "49643fb6ae3f59429959c1a7260b6753",
+    "url": "/compound-channels/static/js/2.eddebabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1bfdd3e22436547add",
+    "url": "/compound-channels/static/js/main.5354f4fb.chunk.js"
+  },
+  {
+    "revision": "7f731d28e73986e9ac73",
+    "url": "/compound-channels/static/js/runtime-main.c4da4961.js"
+  },
+  {
+    "revision": "be1919186dd19943fc503a3bb8dcbe94",
+    "url": "/compound-channels/static/media/add.be191918.svg"
+  },
+  {
+    "revision": "e71bce4d98e84fbc5e0e490650a92d22",
+    "url": "/compound-channels/static/media/borrow.e71bce4d.svg"
+  },
+  {
+    "revision": "ef4578364603b402ea5599d1f0b216bd",
+    "url": "/compound-channels/static/media/checkmark.ef457836.svg"
+  },
+  {
+    "revision": "15a51403a74b4f3270ff30063ff32500",
+    "url": "/compound-channels/static/media/close-money.15a51403.svg"
+  },
+  {
+    "revision": "4ff4debad5f2d70b7bb6e0b24ec03264",
+    "url": "/compound-channels/static/media/close.4ff4deba.svg"
+  },
+  {
+    "revision": "23f0a4e328de1fff8c1644f1b9234ab4",
+    "url": "/compound-channels/static/media/create.23f0a4e3.svg"
+  },
+  {
+    "revision": "66a0ceb4acc0ff8b2c7fa9405f231dcd",
+    "url": "/compound-channels/static/media/dai.66a0ceb4.png"
+  },
+  {
+    "revision": "8710a0a35bee35ea0ce6f9a19dcaa316",
+    "url": "/compound-channels/static/media/forceClose.8710a0a3.svg"
+  },
+  {
+    "revision": "d2afd93a15386bd92059bc77443902ce",
+    "url": "/compound-channels/static/media/inspect.d2afd93a.svg"
+  },
+  {
+    "revision": "c652593c50c9b428e972d274f83bcd29",
+    "url": "/compound-channels/static/media/piggy-bank.c652593c.svg"
+  },
+  {
+    "revision": "51793d5dfff18ea2ec12cdafcc308238",
+    "url": "/compound-channels/static/media/repay.51793d5d.svg"
+  },
+  {
+    "revision": "3092c18293c348960704778a45c91f2e",
+    "url": "/compound-channels/static/media/sign.3092c182.svg"
+  },
+  {
+    "revision": "8d7fd3c0098392e88f8603bf7712435e",
+    "url": "/compound-channels/static/media/supply.8d7fd3c0.svg"
+  },
+  {
+    "revision": "89aeed5b171004ec55cb4f066bcbfb0e",
+    "url": "/compound-channels/static/media/usdc.89aeed5b.png"
+  },
+  {
+    "revision": "fb0bde4523930fe00a23e5a093c4e61b",
+    "url": "/compound-channels/static/media/xmark.fb0bde45.svg"
+  }
+]);
